955c4102daa8e10a7fc8c8914c9905facb3d9c57
bliss administrator <blissadm@ting.esrf.fr>
2020-06-27 21:19:43 +0200

    Fixed MultiplePositions
    Feedback on gui if changes from bliss session. Display which initial position is
    Added first version of id10 diffractometer

---
+++ HardwareObjects/ESRF/ID10Diffractometer.py
@@ -0,0 +1,103 @@
+#
+#  Project: MXCuBE
+#  https://github.com/mxcube.
+#
+#  This file is part of MXCuBE software.
+#
+#  MXCuBE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MXCuBE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import gevent
+import time
+
+from HardwareRepository.HardwareObjects.GenericDiffractometer import (
+    GenericDiffractometer
+)
+from HardwareRepository.HardwareObjects import sample_centring
+from HardwareRepository.BaseHardwareObjects import HardwareObject
+from HardwareRepository import HardwareRepository as HWR
+
+__credits__ = ["MXCuBE collaboration"]
+__version__ = "2.2."
+__status__ = "Draft"
+
+class ID13Diffractometer(GenericDiffractometer):
+
+    # CENTRING_MOTORS_NAME = [
+    #     "phi",
+    #     "phiz",
+    #     "phiy",
+    #     "sampx",
+    #     "sampy",
+    #     "kappa",
+    #     "kappa_phi",
+    #     "beam_x",
+    #     "beam_y",
+    #     "zoom",
+    # ]
+    # This is used if self.centring_motors_list = eval(self.getProperty("centring_motors")) fails
+    
+    # def __init__(self, *args):
+    #     GenericDiffractometer.__init__(self, *args)
+
+    #     # Hardware objects ----------------------------------------------------
+    #     self.zoom_motor_hwobj = None
+    #     self.omega_reference_motor = None
+    #     self.centring_hwobj = None
+    #     self.minikappa_correction_hwobj = None 
+    def init(self):
+
+        self.pixels_per_mm_x = 0
+        self.pixels_per_mm_y = 0
+
+        GenericDiffractometer.init(self)
+
+        self.centring_methods = {
+            GenericDiffractometer.CENTRING_METHOD_MANUAL: self.id13_manual_centring,
+            GenericDiffractometer.CENTRING_METHOD_AUTO: self.start_automatic_centring,
+            GenericDiffractometer.CENTRING_METHOD_MOVE_TO_BEAM: self.start_move_to_beam,
+        }
+
+    def get_pixels_per_mm(self):
+        self.update_zoom_calibration()
+        return GenericDiffractometer.get_pixels_per_mm(self)
+    
+    def update_zoom_calibration(self):
+        """
+        """
+        if "zoom" not in self.motor_hwobj_dict:
+            # not initialized yet
+            return
+
+        zoom_motor = self.motor_hwobj_dict["zoom"]
+
+        props = zoom_motor.getCurrentPositionProperties()
+
+        if "pixelsPerMmZ" in props.keys() and "pixelsPerMmY" in props.keys():
+            self.pixels_per_mm_x = float(props["pixelsPerMmY"])
+            self.pixels_per_mm_y = float(props["pixelsPerMmZ"])
+        else:
+            self.pixels_per_mm_x = 0
+            self.pixels_per_mm_y = 0
+
+        if "beamPositionX" in props.keys() and "beamPositionY" in props.keys():
+            self.beam_xc = float(props["beamPositionX"])
+            self.beam_yc = float(props["beamPositionY"])
+
+        if 0 not in [self.pixels_per_mm_x, self.pixels_per_mm_y]:
+            self.emit(
+                "pixelsPerMmChanged", ((self.pixels_per_mm_x, self.pixels_per_mm_y),)
+            )
+    def get_centred_point_from_coord(self, coord_x, coord_y, return_by_names=True):
+        pass
--- HardwareObjects/MultiplePositions.py
+++ HardwareObjects/MultiplePositions.py
@@ -88,12 +88,12 @@ SIGNAL
     parameter:      state
     description:    send the new state of the object when it changes
 
-    name:           noPosition
+    name:           no_position
     parameter:      None
     description:    sent when after a position change of any of the motor
                     the object is not in any of the predefined positions
 
-    name:           positionReached
+    name:           position_reached
     parameter:      positionName
     description:    sent when after a position change of any of the motor
                     the object has reach a predefined position.
@@ -159,17 +159,17 @@ class MultiplePositions(Equipment):
         motors = self["motors"]
 
         self.motor_hwobj_list = []
-        print(f"@@@@@@@@@@@@@@@@@@@@@@ motors {motors} {type(motors)} is None - {motors is None} - {motors.objectsNames()}")
+        #print(f"@@@@@@@@@@@@@@@@@@@@@@ motors {motors} {type(motors)} is None - {motors is None} - {motors.objectsNames()}")
         for mot in motors:
                 name = mot.getProperty("name")
                 temp_motor_hwobj = self.getObjectByRole(name)
                 self.motor_hwobj_list.append(temp_motor_hwobj)
-                print(f"@@@@@@@@@@@@@@@@@@@@@@ motors  name {name} motor {id(temp_motor_hwobj)}")
+        #        print(f"@@@@@@@@@@@@@@@@@@@@@@ motors  name {name} motor {id(temp_motor_hwobj)}")
 
         #self.roles = motors.getRoles()
         self.roles = self.getRoles()
         tmp = self.getObjectByRole("zoom")
-        print(f"@@@@@@@@@@@@@@@@@@@@@@ tmp {tmp} {type(tmp)}")
+        #print(f"@@@@@@@@@@@@@@@@@@@@@@ tmp {tmp} {type(tmp)}")
         self.deltas = {}
         try:
             # WARNING self.deltas is a LINK to the INTERNAL properties dictionary
@@ -183,44 +183,44 @@ class MultiplePositions(Equipment):
         self.positions_names_list = []
         try:
             positions = self["positions"]
-            print(f"@@@@@@@@@@@@@@@@@@@@@@ positions {positions} {type(positions)}")
+            #print(f"@@@@@@@@@@@@@@@@@@@@@@ positions {positions} {type(positions)}")
         except BaseException:
             logging.getLogger().error("No positions.")
         else:
             for position in positions:
                 name = position.getProperty("name")
-                print(f"name {name}")
+                #print(f"name {name}")
                 if name is not None:
                     self.positions_names_list.append(name)
                     self.positions[name] = {}
 
                     motpos = position.getProperties()
                     motroles = list(motpos.keys())
-                    print(f"motpos {motpos}")
-                    print(f"motroles {motroles}")
+                    #print(f"motpos {motpos}")
+                    #print(f"motroles {motroles}")
 
                     for role in self.roles:
-                        print(f"role {role}")
+                        #print(f"role {role}")
                         self.positions[name][role] = motpos[role]
-                        print(f"self.positions[{name}][{role}] - {self.positions[name][role]}")
+                        #print(f"self.positions[{name}][{role}] - {self.positions[name][role]}")
                 else:
                     logging.getLogger().error("No name for position.")
 
         self.motors = {}
-        print(f"@@@@@@@@@@@@@@@@@@@@@@  motors {type(motors)}")
-        print(f"@@@@@@@@@@@@@@@@@@@@@@  self.roles {self.roles}")
+        #print(f"@@@@@@@@@@@@@@@@@@@@@@  motors {type(motors)}")
+        ##print(f"@@@@@@@@@@@@@@@@@@@@@@  self.roles {self.roles}")
         #for mot in self["motors"]:
         for mot in self.motor_hwobj_list:
-            print(f"@@@@@@@@@@@@@@@@ mot - {mot}")
+            #print(f"@@@@@@@@@@@@@@@@ mot - {mot}")
             #self.motors[mot.getMotorMnemonic()] = mot
             self.connect(mot, "moveDone", self.checkPosition)
             self.connect(mot, "valueChanged", self.checkPosition)
             self.connect(mot, "stateChanged", self.stateChanged)
 
         for key, value in self.positions.items():
-            print(f"key {key} value {value}")
+            #print(f"key {key} value {value}")
             #for key2, value2 in value.items():
-        print(f"$$$$$$$$$$$$$$$ self.positions {self.positions} ")
+        #print(f"$$$$$$$$$$$$$$$ self.positions {self.positions} ")
 
     def get_positions_names_list(self):
         return self.positions_names_list
@@ -246,17 +246,17 @@ class MultiplePositions(Equipment):
         self.checkPosition()
 
     def moveToPosition(self, name, wait=False):
-        print(f"$$$$$$$$$$$$$$$ moveToPosition {name} ")
+        #print(f"$$$$$$$$$$$$$$$ moveToPosition {name} ")
 
         move_list = []
         for role in self.roles:
             device = self.getObjectByRole(role)
             pos = self.positions[name][role]
             move_list.append((device, pos))
-        print(f"$$$$$$$$$$$$$$$ moveToPosition  move_list {move_list} ")
+        #print(f"$$$$$$$$$$$$$$$ moveToPosition  move_list {move_list} ")
         for mot, pos in move_list:
             if mot is not None:
-                print(f"$$$$$$$$$$$$$$$ moveToPosition  mot.set_value(pos) {mot} {pos} ")
+                #print(f"$$$$$$$$$$$$$$$ moveToPosition  mot.set_value(pos) {mot} {pos} ")
                 mot.set_value(pos)
 
         if wait:
@@ -272,18 +272,25 @@ class MultiplePositions(Equipment):
         It checks the positions of all the 'role' motors
         If all of them are within +/- delta tolerance, return pos name
         """
+        #print(f"$$$$$$$$$$$$$$$MULTIPOSHWR get_value ")
         if not self.is_ready():
+            #print(f"$$$$$$$$$$$$$$$MULTIPOSHWR get_value - not self.is_ready() ")
             return None
 
         for posName, position in self.positions.items():
             findPosition = 0
 
             for role in self.roles:
+                #print(f"$$$$$$$$$$$$$$$MULTIPOSHWR get_value  - role {role} ")
                 pos = position[role]
-                mot = self.getDeviceByRole(role)
-
+                role_str = "\"" + str(role) + "\""
+                mot = self.getObjectByRole(role)
+                #print(f"$$$$$$$$$$$$$$$MULTIPOSHWR mot  - {type(mot)} -")
+                
+                #print(f"$$$$$$$$$$$$$$$MULTIPOSHWR get_value  - pos {pos} -")
                 if mot is not None:
                     motpos = mot.get_value()
+                    #print(f"$$$$$$$$$$$$$$$MULTIPOSHWR get_value  - motpos {motpos} -")
                     try:
                         if (
                             motpos < pos + self.deltas[role]
@@ -300,17 +307,17 @@ class MultiplePositions(Equipment):
 
     def checkPosition(self, *args):
         if not self.is_ready():
-            print(f"checkPosition not self.is_ready() { self.is_ready()}")
+            #print(f"checkPosition not self.is_ready() { self.is_ready()}")
             return None
 
         posName = self.get_value()
 
         if posName is None:
-            self.emit("noPosition", ())
+            self.emit("no_position", ())
             return None
         else:
-            self.emit("positionReached", (posName,))
-            print(f"checkPosition emit(positionReached) {posName}")
+            self.emit("position_reached", (posName,))
+            #print(f"checkPosition emit(position_reached) {posName}")
             return posName
 
     def setNewPositions(self, name, newPositions):
